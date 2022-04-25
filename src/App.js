e594aa322b1bcc77a1298cf8d1a98850e76089f8
chrislee1996 <prepchrislee@gmail.com>
2022-04-25 09:56:27 -0400

    calls in app for subjects created

--- src/App.js
+++ src/App.js
@@ -18,6 +18,21 @@ import MineCourses from './components/courses/MineCourses'
 //subjects
 import Art from './components/subjects/Art'
 import Business from './components/subjects/Business'
+import DataAnalysis from './components/subjects/DataAnalysis'
+import Design from './components/subjects/Design'
+import Education from './components/subjects/Education'
+import Engineering from './components/subjects/Engineering'
+import Healthcare from './components/subjects/Healthcare'
+import History from './components/subjects/History'
+import Language from './components/subjects/Language'
+import Law from './components/subjects/Law'
+import Literature from './components/subjects/Literature'
+import Math from './components/subjects/Math'
+import Medicine from './components/subjects/Medicine'
+import Programming from './components/subjects/Programming'
+import Science from './components/subjects/Science'
+import SocialScience from './components/subjects/SocialScience'
+
 
 
 const App = () => {
@@ -101,6 +116,64 @@ const App = () => {
 				element={<Business msgAlert={msgAlert} user={user} />}
 			/>
 
+			<Route
+				path='/courses/dataanalysis'
+				element={<DataAnalysis msgAlert={msgAlert} user={user} />}
+			/>			
+			<Route
+				path='/courses/design'
+				element={<Design msgAlert={msgAlert} user={user} />}
+			/>	
+			<Route
+				path='/courses/education'
+				element={<Education msgAlert={msgAlert} user={user} />}
+			/>	
+
+			<Route
+				path='/courses/engineering'
+				element={<Engineering msgAlert={msgAlert} user={user} />}
+			/>	
+			<Route
+				path='/courses/healthcare'
+				element={<Healthcare msgAlert={msgAlert} user={user} />}
+			/>	
+			<Route
+				path='/courses/history'
+				element={<History msgAlert={msgAlert} user={user} />}
+			/>	
+			<Route
+				path='/courses/language'
+				element={<Language msgAlert={msgAlert} user={user} />}
+			/>	
+			<Route
+				path='/courses/law'
+				element={<Law msgAlert={msgAlert} user={user} />}
+			/>	
+			<Route
+				path='/courses/literature'
+				element={<Literature msgAlert={msgAlert} user={user} />}
+			/>	
+			<Route
+				path='/courses/math'
+				element={<Math msgAlert={msgAlert} user={user} />}
+			/>	
+			<Route
+				path='/courses/medicine'
+				element={<Medicine msgAlert={msgAlert} user={user} />}
+			/>	
+			<Route
+				path='/courses/programming'
+				element={<Programming msgAlert={msgAlert} user={user} />}
+			/>	
+			<Route
+				path='/courses/science'
+				element={<Science msgAlert={msgAlert} user={user} />}
+			/>	
+			<Route
+				path='/courses/socialscience'
+				element={<SocialScience msgAlert={msgAlert} user={user} />}
+			/>	
+			{/* end of subjects */}
 			<Route
 				path='/courses/:id'
 				element={<ShowCourse msgAlert={msgAlert} user={user} />}
